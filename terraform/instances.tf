866c875cf9aeba805747122fa5c0f3a1c6181087
mschirbel <mschirbel.g@gmail.com>
2019-05-12 14:43:51 -0300

    errors in region

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "nat" {
     ami = "ami-30913f47" # this is a special ami preconfigured to do NAT
-    availability_zone = "eu-west-1a"
+    availability_zone = "sa-east-1a"
     instance_type = "m1.small"
     key_name = "${var.aws_key_name}"
     vpc_security_group_ids = ["${aws_security_group.nat.id}"]
-    subnet_id = "${aws_subnet.eu-west-1a-public.id}"
+    subnet_id = "${aws_subnet.sa-east-1a-public.id}"
     associate_public_ip_address = true
     source_dest_check = false
 
@@ -15,11 +15,11 @@ resource "aws_instance" "nat" {
 
 resource "aws_instance" "web-1" {
     ami = "${lookup(var.amis, var.aws_region)}"
-    availability_zone = "eu-west-1a"
+    availability_zone = "sa-east-1a"
     instance_type = "m1.small"
     key_name = "${var.aws_key_name}"
     vpc_security_group_ids = ["${aws_security_group.web.id}"]
-    subnet_id = "${aws_subnet.eu-west-1a-public.id}"
+    subnet_id = "${aws_subnet.sa-east-1a-public.id}"
     associate_public_ip_address = true
     source_dest_check = false
 
@@ -31,11 +31,11 @@ resource "aws_instance" "web-1" {
 
 resource "aws_instance" "db-1" {
     ami = "${lookup(var.amis, var.aws_region)}"
-    availability_zone = "eu-west-1a"
+    availability_zone = "sa-east-1a"
     instance_type = "m1.small"
     key_name = "${var.aws_key_name}"
     vpc_security_group_ids = ["${aws_security_group.db.id}"]
-    subnet_id = "${aws_subnet.eu-west-1a-private.id}"
+    subnet_id = "${aws_subnet.sa-east-1a-private.id}"
     source_dest_check = false
 
     tags {
--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "eu-west-1a-public" {
+resource "aws_subnet" "sa-east-1a-public" {
     vpc_id = "${aws_vpc.main_vpc.id}"
 
     cidr_block = "${var.public_subnet_cidr}"
