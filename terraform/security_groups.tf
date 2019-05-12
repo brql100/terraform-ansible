20cd91cda58013c86c4b2bdc51ae3f30c2d7143e
mschirbel <mschirbel.g@gmail.com>
2019-05-12 13:56:47 -0300

    adding sg for public instance

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -58,3 +58,40 @@ resource "aws_security_group" "nat" {
         Name = "NATSG"
     }
 }
+
+resource "aws_security_group" "web" {
+    name = "vpc_web"
+    description = "Allow incoming HTTP connections."
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress { # MySQL
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        cidr_blocks = ["${var.private_subnet_cidr}"]
+    }
+
+    vpc_id = "${aws_vpc.main_vpc.id}"
+
+    tags {
+        Name = "WebServerSG"
+    }
+}
