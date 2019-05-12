957d445034ece0151db481e74d9fbccfdd76b0af
mschirbel <mschirbel.g@gmail.com>
2019-05-12 13:58:30 -0300

    adding private instance + sg

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -28,3 +28,17 @@ resource "aws_instance" "web-1" {
         Name = "Web Server 1"
     }
 }
+
+resource "aws_instance" "db-1" {
+    ami = "${lookup(var.amis, var.aws_region)}"
+    availability_zone = "eu-west-1a"
+    instance_type = "m1.small"
+    key_name = "${var.aws_key_name}"
+    vpc_security_group_ids = ["${aws_security_group.db.id}"]
+    subnet_id = "${aws_subnet.eu-west-1a-private.id}"
+    source_dest_check = false
+
+    tags {
+        Name = "DB Server 1"
+    }
+}
--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -95,3 +95,47 @@ resource "aws_security_group" "web" {
         Name = "WebServerSG"
     }
 }
+
+resource "aws_security_group" "db" {
+    name = "vpc_db"
+    description = "Allow incoming database connections."
+
+    ingress { # MySQL
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        security_groups = ["${aws_security_group.web.id}"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["${var.vpc_cidr}"]
+    }
+    ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["${var.vpc_cidr}"]
+    }
+
+    egress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    vpc_id = "${aws_vpc.main_vpc.id}"
+
+    tags {
+        Name = "DBServerSG"
+    }
+}
