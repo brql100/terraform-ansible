c0a20c3185d5acba1aa858faeda322642c1fe432
mschirbel <mschirbel.g@gmail.com>
2019-05-12 13:43:31 -0300

    adding variables file

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "aws_access_key" {
+
+}
+variable "aws_secret_key" {
+
+}
+variable "aws_key_path" {
+
+}
+variable "aws_key_name" {
+
+}
+
+variable "aws_region" {
+    description = "EC2 Region for the VPC"
+    default = "eu-west-1" # usando EU para testes.
+}
+
+variable "amis" {
+    description = "AMIs by region"
+    default = {
+        eu-west-1 = "ami-66506c1c"
+    }
+}
+
+variable "vpc_cidr" {
+    description = "CIDR for the whole VPC"
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+    description = "CIDR for the Public Subnet"
+    default = "10.0.0.0/24"
+}
+
+variable "private_subnet_cidr" {
+    description = "CIDR for the Private Subnet"
+    default = "10.0.1.0/24"
+}
